207c812f5569db6b56a2765096a3ef9d84bae307
naomi <naomilounsbury@hotmail.com>
2022-01-21 10:20:46 -0800

    deleted some files but they aren't deleted

--- routes/apiroutes/index.js
+++
@@ -1,7 +0,0 @@
-//importing the notes routes and setting up the router to use those routes
-const router = require('express').Router();
-const notesRoutes = require('.notesRoutes');
-
-router.use(notesRoutes);
-
-module.exports = router;
